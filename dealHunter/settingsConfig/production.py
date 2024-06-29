6c6651596cc5d38b65f6edbaaecf26d02a27278a
Ahmed <ahmeddragon700@gmail.com>
2024-06-29 17:05:41 +0300

    fix settings

--- dealHunter/settingsConfig/production.py
+++ dealHunter/settingsConfig/production.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = "dealHunter.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [BASE_DIR / "templates",'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
